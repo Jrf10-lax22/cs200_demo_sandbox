db55afd7571c18c579799981b8be191aa35b71ea
Funk <1020jrf@gmail.com>
2026-03-27 10:28:49 -0400

    Changed input name

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def func1():
     lst = []
     print('Enter numbers one at a time. Enter 0 to stop.')
-    x = int(input('num:'))
-    while x != 0:
-        lst.append(x)
-        x = int(input('num:'))
+    inpnum = int(input('num:'))
+    while inpnum != 0:
+        lst.append(inpnum)
+        inpnum = int(input('num:'))
     return lst
 
 def func2(data):
